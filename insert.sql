467b2b780753637b657aa57b5a01b2befc2ebeba
HringurH <hringur.hrafnkelsson@gmail.com>
2025-11-27 11:49:12 +0000

    Started on inserts

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO eras (name, abbreviation, description, absolute_start_year) VALUES
+('Days before days', 'DBD', 'The timeless era before Arda’s shaping; the Ainur dwell with Ilúvatar and sing the world into being.', -10000),
+('Years of the Lamps', 'YOL', 'The Valar shape Arda; two great Lamps light the world until destroyed by Melkor.', 0),
+('Years of the Trees', 'YOT', 'In Valinor the Two Trees give light; Elves awaken, kingdoms rise, and Melkor steals the Silmarils and destroys the Trees.', 1000),
+('Years of the Sun', 'YOS', 'The Sun and Moon first rise; most history of Elves and Men unfolds, culminating in Númenor’s rise and fall and the Last Alliance.', 5000);
+('First Age', 'FA', 'Wars against Morgoth; the Silmarils drive great tragedy; ends with Morgoth’s defeat.', 10000)
+('Second Age', 'SA', 'Númenor flourishes then falls; the Rings of Power are forged; Sauron is first defeated by the Last Alliance.', 20000)
+('Third Age', 'TA', 'Decline of Elves and Númenórean realms; Sauron returns; ends with the War of the Ring and the One Ring’s destruction.', 30000)
+('Fourth Age', 'FOA', 'The Dominion of Men; magic and Elves fade; Middle-earth becomes the world of later human history.', 40000);
